9d1066b741e524ff28a3276804c492a812ad9750
Dean Peters <peters.dean@gmail.com>
2012-04-26 11:14:01 -0400

    first commit

---
+++ NewsFeed.SectionPopulate.js
@@ -0,0 +1,182 @@
+(function($) {
+    
+    /* 
+     * formatting for this plugin was quickly generate & started with:
+     *   http://starter.pixelgraphics.us/
+     *
+     * of course, you still gotta write the code ...
+     */
+    if (!$.NewsFeed) {
+        $.NewsFeed = new Object();
+    };
+
+    $.NewsFeed.SectionPopulate = function(el, sectionName, rssCategory, maxStories, titleSelector, listSelector, toggleClass, options) {
+        // To avoid scope issues, use 'base' instead of 'this'
+        // to reference this class from internal events and functions.
+        var base = this;
+
+        // Access to jQuery and DOM versions of element
+        base.$el = $(el);
+        base.el = el;
+
+        // Add a reverse reference to the DOM object
+        base.$el.data("NewsFeed.SectionPopulate", base);
+
+        base.init = function() {
+            if (typeof(sectionName) === "undefined" || sectionName === null) sectionName = "Top Stories";
+            if (typeof(rssCategory) === "undefined" || rssCategory === null) rssCategory = "topstories";
+            if (typeof(maxStories) === "undefined" || maxStories === null) maxStories = 12;
+            if (typeof(listSelector) === "undefined" || listSelector === null) listSelector = "ul";
+            if (typeof(titleSelector) === "undefined" || titleSelector === null) titleSelector = "h4";
+            if (typeof(toggleClass) === "undefined" || toggleClass === null) toggleClass = "on";
+
+            base.sectionName = sectionName;
+            base.rssCategory = rssCategory;
+            base.maxStories = maxStories;
+            base.listSelector = listSelector;
+            base.titleSelector = titleSelector;
+            base.toggleClass = toggleClass;
+
+            base.options = $.extend({}, $.NewsFeed.SectionPopulate.defaultOptions, options);
+
+            // Put your initialization code here (or "this is where it all happens")
+            // base.test();
+            base._getData();
+        };
+
+        // Sample Function, Uncomment to use
+        // base.functionName = function(paramaters){
+        //
+        // };
+        
+        
+        /* 
+        * this (quasi-protected) method reaches out to the YQL and gets data from yahoo news rss feeds 
+        * it needs to be modified to incorporate feeds from other sources as well
+        * 
+        */         
+        base._getData = function() {
+            
+            yqlLimit = (base.maxStories && base.maxStories > 0) ? '%20limit%20' + base.maxStories : '';
+
+            yqlURL = "http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20rss%20where%20url%20in%20(%0A%20%20%20%20'http%3A%2F%2Fnews.yahoo.com%2Frss%2F" + base.rssCategory + "'%0A)%20%20and%20content.type%20%3D%20'image%2Fjpeg'" + yqlLimit + "%20%7C%20sort(field%3D%22date%22%2C%20descending%3D%22true%22)%20%20%7C%20unique(field%3D%22guid%22)%20&format=json&diagnostics=true&callback=?";
+            console.log(yqlURL);
+
+            var jqxhr = $.getJSON(yqlURL, function(data, status) {
+                console.log("success1:", status, data);
+                base._publishStories(data.query.results);
+            }).success(function(data, status) {
+                console.log("2:", status);
+            }).error(function(data, status) {
+                console.log("error:", status, data);
+            }).complete(function(data, status) {
+                console.log("complete:", status, data);
+            });
+
+        };
+
+
+        /* 
+         * this module takes what was retreived from _getData() and renders it
+         *
+         * it needs to be expaned a bit for those RSS instances where the
+         * yahoo media rss extension is NOT used for images
+         *
+         */
+        base._publishStories = function(items) {
+
+            $(base.$el.children(base.titleSelector + ":first")).text(base.sectionName);
+
+            _origUl = $(base.$el.children(base.listSelector + ":first"));
+            _ul = _origUl.clone();
+            $.each(items.item, function(ii, item) {
+
+                /* this line here will limit stories for non YQL queries */
+                if(ii >= base.maxStories) return false;
+                
+
+                /* create the text link first for use & for cloning */
+                _href = $('<div />').append(
+                $('<a/>', {
+                    href: item.link,
+                    text: item.title,
+                    title: item.title
+                }));
+
+                /* if an image exists, clone the text link, replacing text w/image */
+                _img = $('<div />');
+                if (item.content && item.content.url) {
+                    _imgObj =
+                    _img = _href.clone();
+                    _img.children('a').html(
+                    $('<img/>', {
+                        src: item.content.url,
+                        // height: (item.content.height * 1.5),
+                        // width: (item.content.url * 1.5),
+                        alt: item.title
+                    }));
+                }
+
+                /* add the new link to the cloned <ul> */                
+                _li = $('<li />').append(_img).append(_href);
+                _ul.append(_li);
+                
+            });
+            
+            /*
+             * now take what's in memory (the cloned <ul>) & replace what's on screen
+             *
+             * you may want to consider a transition here
+             *
+             * fyi, what this does in terms of the plugin is:
+             * $(base.$el.children("ul:first")).replaceWith(_ul);
+             */
+            _origUl.replaceWith(_ul);    
+
+        };
+
+
+        base.test = function() {
+
+            $(base.$el.children("h4:first")).text(base.sectionName);
+
+            // let's de-ref the UL in case we want to do some fancy .html() appends    
+            _ul = $(base.$el.children("ul:first"));
+            _ul.append("<li>foo</li>");
+
+        };
+
+        // Run initializer
+        base.init();
+    };
+
+    /* set the defaults */
+    $.NewsFeed.SectionPopulate.defaultOptions = {
+        sectionName: "Top Stories",
+        rssCategory: "topstories",
+        maxStories: 12,
+        titleSelector: "h4",
+        listSelector: "ul",
+        toggleClass: "on"
+    };
+
+    /* 
+    * here is your interface, here is an exmple syntax:
+    *  $(function(){
+    *    $('#sports').newsfeed_SectionPopulate('Sports Stories', 'sports', 12, 'h4', 'ul', 'on');
+    *    $('#living').newsfeed_SectionPopulate('Living', 'living', 5, 'h3');
+    *  });
+    */
+    $.fn.newsfeed_SectionPopulate = function(sectionName, rssCategory, maxStories, titleSelector, listSelector, toggleClass, options) {
+        return this.each(function() {
+            (new $.NewsFeed.SectionPopulate(this, sectionName, rssCategory, maxStories, titleSelector, listSelector, toggleClass, options));
+        });
+    };
+
+    // This function breaks the chain, but returns
+    // the NewsFeed.SectionPopulate if it has been attached to the object.
+    $.fn.getNewsFeed_SectionPopulate = function() {
+        this.data("NewsFeed.SectionPopulate");
+    };
+
+})(jQuery); ​
---
+++ README.md
@@ -0,0 +1,54 @@
+NewsFeed.SectionPopulate.js - a jQuery plugin that renders RSS items from jSON created via YQL
+===============================================================================================
+
+NewsFeed.SectionPopulate.js is a jQuery plugin used to consume and render RSS news items via jSON files created via YQL.
+
+While the jSON files demonstreated here come by means of YQL courtesly of the Yahoo! Developer Network, this plugin can be expanded to consume any jSON feed providing similarly structured data.
+
+## Implementation ##
+
+	<head>
+		...
+		<script type='text/javascript' src='https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>
+		<script type='text/javascript' src='https://YOURDOMAIN.COM/YOURJSPATH/NewsFeed.SectionPopulate.js'></script>
+		...
+		<script type='text/javascript'>
+			$(window).load(function() {
+				$(function(){
+					/* this will render the first 5 stories in Yahoo!'s Top Stories News RS feed via YQL */
+					$('#topStories').newsfeed_SectionPopulate('My Top Stories', 'topstories', 5, 'h4', 'ul', 'on');
+					});
+				});
+		</script>
+		...
+	</head>
+	<body>
+		...
+		<div id="topStories">
+    		<h4></h4>
+    		<ul id="thelist"></ul>
+		</div>
+		...
+	</body>
+
+
+## To Do ##
+
+
+# add (more robust) error handling for ajax
+# add google analytics, w/push for outbound clicks
+# perhaps use data- item attributes instead of parameters
+# use session storage for json captures so only 1 trip required per session
+# how about feeds from sources other than Yahoo! News
+# error handling, more of it for both AJAX calls and instatiating settings
+# roll into phonegap project
+
+## Technologies Employed ##
+
+Here's a list of the technologies that make this plugin work:
+
+* jQuery - peferably v 1.5 or higher, though this might work on 1.3.2 (not tested)
+* HTML - though HTML5 is a good way to go, and CSS3 if you're dealing with media queries
+* Yahoo! Query Language - though you can use any service that renders similar jSON structures 
+* CORS - that stands for cross-origin resource sharing, which YQL has enabled (thanks!-)
+
